102534ba88454655408784f6156f7720b9b20a29
t0maslb <tomaslb@tutanota.com>
2020-04-24 14:35:38 +0200

    organized files for gnu stow. TODO add stow to install.sh, update README and remove colorscheme slots in st

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-sudo pacman -Syy --noconfirm chromium neovim git wget neofetch exa youtube-dl zsh code-oss vim sxiv zathura ranger ttf-joypixels ttf-fira-code xclip xfce4-power-manager dunst zathura-pdf-mupdf
+sudo pacman -Syy --noconfirm chromium neovim git wget neofetch exa youtube-dl zsh code-oss vim sxiv zathura ranger ttf-joypixels ttf-fira-code xclip xfce4-power-manager dunst zathura-pdf-mupdf alsa-utils thunar materia-gtk-theme
 cd ~
 mkdir Software
 cd Software
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
-yay -S --noconfirm ttf-iosevka xcolor ttf-material-design-icons-git libxft-bgra
+yay -S --noconfirm nerd-fonts-iosevka xcolor ttf-material-design-icons-git libxft-bgra
